965b3f9814da962f36e3c474b6a9b52a9903b100
srajan07 <tiwarisrajan07@gmail.com>
2026-07-07 16:44:38 +0530

    Implement question CRUD operations

--- controllers/createQuestion.js
+++ controllers/createQuestion.js
@@ -14,13 +14,16 @@ async function createQuestion(req,res){
         message:"Question already exist"
     })
    }
-   await Question.create({
+  const question= await Question.create({
       title,
       description,
       difficulty,
       category,
    });
-   return res.status(201).json("Question created");
+   return res.status(201).json({
+    message:"Question created successfully",
+    question,
+   });
 }
 catch(error){
     return res.status(500).json({
@@ -34,15 +37,40 @@ async function readQuestion(req,res){
   if (questions.length === 0){
     return res.status(404).json("Question not founded");
   }
- return res.json({
-    title:questions.title,
-    description:questions.description,
-    difficulty:questions.difficulty,
-    category:questions.category,
- });
+ return res.status(200).json(questions);
 }
+//update the question
+async function updateQues(req,res){
+    const {id} =req.params ;
+    const data = req.body;
+    const updateq = await Question.findByIdAndUpdate(id,data,{new :true}); 
+    if(!updateq){
+        return res.status(404).json({
+            message:"Question not founded"
+        })
+    }
+    return res.status(200).json({
+        message:"Question updated Succesfully",
+        question:updateq
 
+    });
+}
+async function deleteQues(req,res){
+    const {id}=req.params;
+    const deleteq=await Question.findByIdAndDelete(id);
+    if(!deleteq){
+        return res.status(404).json({
+        message:"Internal sever error",
+    })
+    }
+    return res.status(200).json({
+            message:"delete question successfully",
+
+        })
+}
 module.exports={
     createQuestion,
     readQuestion,
+    updateQues,
+    deleteQues,
 }
